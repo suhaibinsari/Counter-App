c51d1aa2011efd8b6e93a7a504da194c47f6ee58
suhaibinsari <mstar390@gmail.com>
2022-03-01 21:52:15 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Appui from './components/Appui/Appui'
+import Functions from './components/functions/Functions'
+
+
+function App() {
+  return (
+    <div>
+<Appui/>
+<Functions/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div>moon</div>
+  )
+}
+
+export default Button
